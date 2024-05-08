6cad40eec86710ec7a72e260bd6a85256862538c
paxtonBravo <96046448+paxtonBravo@users.noreply.github.com>
2024-05-08 18:21:25 +0800

    更換 twstock 依賴成 stock

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
 import json
 from typing import List
 from datetime import datetime, timezone, timedelta
+from stock.real_time import stock, index
 import threading
 import requests
 import twstock
 import time
 import os
-
-
-def get_index_data():  # 取得台股指數報價
-    index_url = "https://mis.twse.com.tw/stock/data/mis_ohlc_TSE.txt"
-    futures_index_url = "https://mis.twse.com.tw/stock/data/futures_side.txt"
-    index_req = json.loads(requests.get(index_url).text)
-    futures_index_req = json.loads(requests.get(futures_index_url).text)
-    return {"index": index_req, "futures_index": futures_index_req}
-
-
-def get_real_time_stock_data(code: str | List[str]) -> dict:  # 盤中即時報價
-    stock = twstock.realtime.get(code)
-
-    def transform_data(stock: dict) -> dict:
-        real_time = stock["realtime"]
-        info = stock["info"]
-        latest_trade_price = real_time["latest_trade_price"]  # 即時報價
-        five_price = real_time["best_bid_price"]  # 五檔報價
-
-        return {
-            "code": info["code"],
-            "name": info["name"],
-            "time": info["time"],
-            "timestamp": stock["timestamp"],
-            "price": {
-                "now": (
-                    latest_trade_price if latest_trade_price != "-" else five_price[0]
-                ),
-                "open": real_time["open"],
-                "high": real_time["high"],
-                "low": real_time["low"],
-            },
-        }
-
-    def multiple_data_to_list(stock: dict) -> dict:
-        del stock["success"]
-        return [stock[key] for i, key in enumerate(stock)]
-
-    if type(code) == str:
-        return [transform_data(stock)]
-    else:
-        return [transform_data(data) for data in multiple_data_to_list(stock)]
 
 
 def loading(keep: int, speed: int = 0.2):
@@ -71,8 +30,8 @@ def display(data: str | List[dict], delay: int) -> None:
             th.start()
 
             # 更新主要資料
-            stock_data = get_real_time_stock_data(data)
-            index_data = get_index_data()
+            stock_data = stock(data)
+            index_data = index()
 
             result_time = f"""最後更新時間: {str(datetime.now(
                 timezone(timedelta(hours=+8))).strftime("%Y/%m/%d %H:%M:%S"))}\n\n"""
